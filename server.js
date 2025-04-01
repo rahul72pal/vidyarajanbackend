12937f7f16878b35742376772dc584b8df57b04b
Rahul <rahulgwl72@gmail.com>
2025-04-01 21:16:44 +0530

    update db password

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const db = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: null,
   database: process.env.DB_NAME,
 });
 
